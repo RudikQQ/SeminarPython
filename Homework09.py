f4a14fade947917ed702375793794f1f14ff00f1
Rudik <lexa.arsenal.rudik@gmail.com>
2023-06-29 09:48:58 +0300

    Добавили файлы

--- Homework09.py
+++ Homework09.py
@@ -4,20 +4,20 @@ import pandas as pd
 # sample_data. Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population)
 
                     #decision
-​
+
 df[df['population']<501]['median_house_value'].agg(['mean'])
                     # Вариант 2:
-​
+
 df[df['population']<501]['median_house_value'].mean()
 206799.95140186916
                     # Task42
 # Узнать какая максимальная households в зоне минимального значения population
 
                     #decision
-​
+
 df[df['population']==df['population'].min()]['households'].max()
 
-​
+
                     # Вариант 2:
 df[df['population']==df['population'].min()]['households'].agg(['max'])
 
